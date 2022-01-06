eeafb3e47994cc0bef66f1b227cfbccdba9fb36a
superbono <choiar@pincar.co.kr>
2022-01-06 18:38:10 +0900

    vue 수정

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,11 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+
 const store = new Vuex.Store ({
-    state: {
 
-    },
-    getters: {
-
-    },
-    mutations: {
-
-    },
-    actions: {
-
-    }
 });
 
 
